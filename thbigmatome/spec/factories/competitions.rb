5a4f26eba4e1153232fb8245e04481abc7cc9302
whiterochen-73 <necrofantasia1@gmail.com>
2026-02-22 17:16:10 +0900

    fix: competition uniqueness validation spec (spec/models/competition_spec.rb:13)
    Use alpha characters in competition factory name sequence to satisfy
    shoulda-matchers case-sensitivity check for uniqueness validation.

--- thbigmatome/spec/factories/competitions.rb
+++ thbigmatome/spec/factories/competitions.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :competition do
-    sequence(:name) { |n| "大会#{n}" }
+    sequence(:name) { |n| "Competition#{n}" }
     sequence(:year) { |n| 2020 + (n % 10) }
     competition_type { "league_pennant" }
   end

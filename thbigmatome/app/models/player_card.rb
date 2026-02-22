d5da9d15d31dbeba4f30084d399816c05498c9c1
whiterochen-73 <necrofantasia1@gmail.com>
2026-02-22 11:39:34 +0900

    refactor: extract shared validations to BaseballCardValidations concern (cmd_287)

--- thbigmatome/app/models/player_card.rb
+++ thbigmatome/app/models/player_card.rb
@@ -1,4 +1,6 @@
 class PlayerCard < ApplicationRecord
+  include BaseballCardValidations
+
   belongs_to :card_set
   belongs_to :player
   belongs_to :batting_style, optional: true
@@ -11,70 +13,4 @@ class PlayerCard < ApplicationRecord
 
   validates :card_set_id, :player_id, presence: true
   validates :card_set_id, uniqueness: { scope: :player_id }
-
-  DEFENSE_RATING_FORMAT = /\A[0-5][A-ES]\z/.freeze
-  DEFENSE_ATTRIBUTES = %i[
-    defense_p defense_c defense_1b defense_2b defense_3b defense_ss
-    defense_of defense_lf defense_cf defense_rf special_defense_c
-  ].freeze
-
-  validates(*DEFENSE_ATTRIBUTES,
-            format: { with: DEFENSE_RATING_FORMAT, message: :invalid_format },
-            allow_blank: true)
-
-  validates :throwing_c,
-            presence: { message: :required_when_defense_c_present },
-            if: -> { defense_c.present? }
-  validates :throwing_c,
-            numericality: { only_integer: true, message: :not_an_integer },
-            inclusion: { in: -5..5, message: :out_of_range },
-            allow_blank: true
-  validates :special_throwing_c,
-            presence: { message: :required_when_special_defense_c_present },
-            if: -> { special_defense_c.present? }
-  validates :special_throwing_c,
-            numericality: { only_integer: true, message: :not_an_integer },
-            inclusion: { in: -5..5, message: :out_of_range },
-            allow_blank: true
-
-  OUTFIELDER_THROWING_ATTRIBUTES = %i[throwing_of throwing_lf throwing_cf throwing_rf].freeze
-  OUTFIELDER_THROWING_VALUES = %w[S A B C].freeze
-
-  validates(*OUTFIELDER_THROWING_ATTRIBUTES,
-            inclusion: { in: OUTFIELDER_THROWING_VALUES, message: :must_be_s_a_b_or_c },
-            allow_blank: true)
-
-  validates :starter_stamina,
-            numericality: { only_integer: true, message: :not_an_integer },
-            inclusion: { in: 4..9, message: :out_of_range },
-            allow_blank: true,
-            unless: :is_relief_only
-  validates :relief_stamina,
-            numericality: { only_integer: true, message: :not_an_integer },
-            inclusion: { in: 0..3, message: :out_of_range },
-            allow_blank: true
-
-  {
-    defense_of: :throwing_of, defense_lf: :throwing_lf, defense_cf: :throwing_cf, defense_rf: :throwing_rf
-  }.each do |defense_attr, throwing_attr|
-    validates throwing_attr, presence: { message: :required_when_defense_present }, if: -> { send(defense_attr).present? }
-  end
-
-  validates :speed, presence: true, numericality: { only_integer: true, message: :not_an_integer }, inclusion: { in: 1..5, message: :out_of_range }
-  validates :bunt, presence: true, numericality: { only_integer: true, message: :not_an_integer }, inclusion: { in: 1..10, message: :out_of_range }
-  validates :steal_start, presence: true, numericality: { only_integer: true, message: :not_an_integer }, inclusion: { in: 1..22, message: :out_of_range }
-  validates :steal_end, presence: true, numericality: { only_integer: true, message: :not_an_integer }, inclusion: { in: 1..22, message: :out_of_range }
-  validates :injury_rate, presence: true, numericality: { only_integer: true, message: :not_an_integer }, inclusion: { in: 1..7, message: :out_of_range }
-
-  validate :defense_of_exclusivity
-
-  private
-
-  def defense_of_exclusivity
-    has_of = defense_of.present?
-    has_individual = [ defense_lf, defense_cf, defense_rf ].any?(&:present?)
-    if has_of && has_individual
-      errors.add(:base, :of_and_individual_exclusive)
-    end
-  end
 end

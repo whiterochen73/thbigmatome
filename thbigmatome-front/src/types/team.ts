82d8b307cc8ef92810323788af5b3ed4e2e7adeb
whiterochen-73 <necrofantasia1@gmail.com>
2026-02-22 23:25:11 +0900

    fix: Team型にuser_idを追加 cmd_315

--- thbigmatome-front/src/types/team.ts
+++ thbigmatome-front/src/types/team.ts
@@ -1,11 +1,12 @@
 import { type Manager } from '@/types/manager'
 
 export interface Team {
-  id: number;
-  name: string;
-  short_name: string;
-  is_active: boolean;
-  has_season: boolean;
-  director?: Manager;
-  coaches?: Manager[];
+  id: number
+  name: string
+  short_name: string
+  is_active: boolean
+  has_season: boolean
+  user_id?: number | null
+  director?: Manager
+  coaches?: Manager[]
 }

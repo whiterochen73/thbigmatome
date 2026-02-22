ae243c744cac37a98261893b9b6fdfe92451c046
whiterochen-73 <necrofantasia1@gmail.com>
2026-02-22 23:25:05 +0900

    fix: TeamSerializerにuser_idを追加しGameImportView自チーム取得を修正 cmd_315

--- thbigmatome/app/serializers/team_serializer.rb
+++ thbigmatome/app/serializers/team_serializer.rb
@@ -1,5 +1,5 @@
 class TeamSerializer < ActiveModel::Serializer
-  attributes :id, :name, :short_name, :is_active, :has_season
+  attributes :id, :name, :short_name, :is_active, :has_season, :user_id
   # 必要であれば、Team詳細取得時にManager情報も埋め込む
   has_one :director
   has_many :coaches

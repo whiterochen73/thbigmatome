969f71fa3bd107195215efc170d3fd236c6c90a5
whiterochen-73 <necrofantasia1@gmail.com>
2026-03-14 10:41:26 +0900

    feat(shared): add FilterBar.vue with slot-based filter layout

---
+++ thbigmatome-front/src/components/shared/FilterBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row class="mb-2" dense>
+    <v-col v-if="$slots.search" cols="12" sm="4">
+      <slot name="search" />
+    </v-col>
+    <v-col v-if="$slots.filters">
+      <slot name="filters" />
+    </v-col>
+    <v-col v-if="$slots.toggles" class="d-flex justify-end">
+      <slot name="toggles" />
+    </v-col>
+  </v-row>
+</template>
